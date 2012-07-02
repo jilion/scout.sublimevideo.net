139a719550d0da4cf85fd765330a53aebfbd2107
Rémy Coutable <remy@jilion.com>
2012-07-02 15:09:55 +0200

    Add a basic stats page.

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ ScoutSublimevideo::Application.routes.draw do
   get 'new/:day' => 'carousel#new_sites_day', as: 'new_sites_day'
   get 'active/:day' => 'carousel#new_active_sites_week', as: 'new_active_sites_week'
 
+  get 'stats' => 'stats#index', as: 'stats'
+
   root to: redirect("/new/#{I18n.l(Time.now.utc.midnight, format: :Y_m_d)}")
 end
