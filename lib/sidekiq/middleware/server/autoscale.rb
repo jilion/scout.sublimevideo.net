606be4c177d988a90ddb90ad9345e9096ab95209
Rémy Coutable <remy@jilion.com>
2012-07-04 12:26:09 +0200

    Try to set workers to 0 only when the current worker is the last one

--- lib/sidekiq/middleware/server/autoscale.rb
+++ lib/sidekiq/middleware/server/autoscale.rb
@@ -7,13 +7,17 @@ module Sidekiq
       class Autoscale
         def call(worker, msg, queue)
           yield
-          Wrappers::Heroku.workers = 0 if backlog.zero?
+          Wrappers::Heroku.workers = 0 if backlog.zero? && workers.one? && workers.first == worker
         end
 
         def backlog
           Sidekiq.redis do |conn|
             conn.smembers('queues').inject(0) { |sum, q| sum += conn.llen("queue:#{q}") || 0 }
           end
+        end
+
+        def workers
+          Sidekiq.redis { |conn| conn.smembers('workers') }
         end
       end
 
