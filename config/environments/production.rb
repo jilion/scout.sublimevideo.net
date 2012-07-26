b88819f2fa7f521c854cd9897876a8045744a6f1
Rémy Coutable <remy@jilion.com>
2012-07-26 10:59:05 +0200

    Try to redirect Oink to stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 ScoutSublimevideo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.use Oink::Middleware, logger: Rails.logger
+  config.middleware.use Oink::Middleware, logger: Logger.new(STDOUT)
 
   # Code is not reloaded between requests
   config.cache_classes = true
