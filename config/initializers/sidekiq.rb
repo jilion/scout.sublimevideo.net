62d312b35c4921418217423f05a7053ca4c0192a
Rémy Coutable <remy@jilion.com>
2012-11-06 15:29:40 +0100

    Move autoscale to scheduled task

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,15 +6,10 @@ Sidekiq.configure_client do |config|
   config.redis = { namespace: 'scout' }
 
   config.client_middleware do |chain|
-    chain.add Sidekiq::Middleware::Client::Autoscale if Rails.env.production?
     chain.add Sidekiq::Middleware::Client::UniqueJob
   end
 end
 
 Sidekiq.configure_server do |config|
   config.redis = { namespace: 'scout' }
-
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Middleware::Server::Autoscale if Rails.env.production?
-  end
 end
