b9e7065390e7dbf8f81fcc4cbd34fba58cc00974
Rémy Coutable <remy@jilion.com>
2012-12-18 15:10:57 +0100

    Remove the scout namespace and rename use the 'scout' queue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,8 @@
 require 'sidekiq/middleware/client/unique_job'
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'scout' }
 
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::UniqueJob
   end
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'scout' }
 end
