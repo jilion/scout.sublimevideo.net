0639b02ecde8fa755906495ad2c960bd9172fdb4
Rémy Coutable <remy@rymai.me>
2013-04-11 13:12:34 +0200

    Actually fix shit

--- app/workers/screenshots_worker.rb
+++ app/workers/screenshots_worker.rb
@@ -59,7 +59,7 @@ class ScreenshotsWorker
   def _sites_to_activity_screenshot(opts = { plays_threshold: 10, days_interval: 5.days.ago })
     sites = []
     Site.find_each(select: %w[token], with_state: 'active', with_min_billable_video_views: opts[:plays_threshold]) do |site|
-      sites << site if ScreenshotedSite.find_by_t(site.token).latest_screenshot_older_than(opts[:days_interval])
+      sites << site if ScreenshotedSite.where(t: site.token).first.latest_screenshot_older_than(opts[:days_interval])
     end
 
     sites
