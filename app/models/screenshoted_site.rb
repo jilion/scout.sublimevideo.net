d33c54c688a75ec71d319bd2f888f7794f795443
Rémy Coutable <remy@rymai.me>
2013-04-12 12:24:10 +0200

    Fix issue when screenshoted site has no screenshot

--- app/models/screenshoted_site.rb
+++ app/models/screenshoted_site.rb
@@ -57,7 +57,7 @@ class ScreenshotedSite
   #
   # @return [Boolean]
   def latest_screenshot_older_than?(date)
-    screenshots.latest.created_at < date
+    screenshots.latest.nil? || screenshots.latest.created_at < date
   end
 
   def prepare_for_carousel
