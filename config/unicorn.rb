8950a0a99fe849db76aed0209e9839d8a889a26a
Rémy Coutable <remy@rymai.me>
2013-04-18 10:23:42 +0200

    Increase Redis pool

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,18 @@ before_fork do |server, worker|
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 2 } # for web dyno
+  end
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 2 } # for web dyno
   end
 end
