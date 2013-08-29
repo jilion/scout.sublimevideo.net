42d1825b83b72fe035c215ad8976791cb9e1c605
Rémy Coutable <remy@rymai.me>
2013-08-29 12:16:17 +0200

    Use Basic Auth for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,13 @@
 ScoutSublimevideo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.use Rack::DeviseCookieAuth,
-    secret: ENV['COOKIE_SECRET'],
-    resource: 'admin',
-    redirect_to: 'https://admin.sublimevideo.net/login'
+  use Rack::Auth::Basic, "Protected Area" do |username, password|
+    username == 'jilion' && password == ENV['AUTH_PASSWORD']
+  end
+
+  # config.middleware.use Rack::DeviseCookieAuth,
+  #   secret: ENV['COOKIE_SECRET'],
+  #   resource: 'admin',
+  #   redirect_to: 'https://admin.sublimevideo.net/login'
 
   # Code is not reloaded between requests
   config.cache_classes = true
