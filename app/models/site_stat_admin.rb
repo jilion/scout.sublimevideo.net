95cef7ea8b32ff417cb54ee0b095eb2bcd46ef0b
Rémy Coutable <remy@rymai.me>
2013-10-04 11:51:46 +0200

    Simplify stats private API routes

--- app/models/site_stat_admin.rb
+++ app/models/site_stat_admin.rb
@@ -3,9 +3,9 @@ require 'sublime_video_private_api/model'
 class SiteStatAdmin
   include SublimeVideoPrivateApi::Model
   uses_private_api :stats
-  collection_path '/private_api/sites/:site_token/site_admin_stats'
+  collection_path '/private_api/site_admin_stats'
 
   def self.by_hits_for(token)
-    get_raw(:last_pages, _site_token: token)[:parsed_data][:data][:pages]
+    get_raw(:last_pages, site_token: token)[:parsed_data][:data]
   end
 end
