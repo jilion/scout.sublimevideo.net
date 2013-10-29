c1a37892c3ec869ed7f44346a25b34d62d9036d9
Rémy Coutable <remy@rymai.me>
2013-10-29 14:37:38 +0100

    Add asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ ScoutSublimevideo::Application.configure do
   # config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = 'd1bxzixzcnt4ci.cloudfront.net'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
